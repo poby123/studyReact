65152408d636e114b4b647024dac30a6ba0a33d2
poby123 <youngsm12@naver.com>
2020-08-22 19:11:49 +0900

    redux with action

--- src/stories/ReduxStory.jsx
+++ src/stories/ReduxStory.jsx
@@ -10,6 +10,8 @@ import AdvReduxApp02 from '../07/AdvReduxApp02';
 import AdvReduxApp03 from '../07/AdvReduxApp03';
 import AdvReduxApp04 from '../07/AdvReduxApp04';
 import AdvReduxApp05 from '../07/AdvReduxApp05';
+import AdvReduxApp06 from '../07/AdvReduxApp06';
+import AdvReduxApp07 from '../07/AdvReduxApp07';
 
 storiesOf('Redux App', module)
   .addWithJSX('기본 스토어 설정', () => <ReduxApp01 />)
@@ -19,4 +21,6 @@ storiesOf('Redux App', module)
   .addWithJSX('다중 액션 설정', () => <AdvReduxApp02 />)
   .addWithJSX('배열 액션 호출', () => <AdvReduxApp03 />)
   .addWithJSX('그래프DB 변환 작업', () => <AdvReduxApp04 />)
-  .addWithJSX('그래프DB 수정확인', () => <AdvReduxApp05 />);
+  .addWithJSX('그래프DB 수정확인', () => <AdvReduxApp05 />)
+  .addWithJSX('데이터 컴포넌트, 화면 컴포넌트 분리', () => <AdvReduxApp06 />)
+  .addWithJSX('액션 컴포넌트에서 데이터 변경', <AdvReduxApp07 />);
